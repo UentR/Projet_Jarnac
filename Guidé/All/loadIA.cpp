332c2c79fcd4a74df85e3cc2c085aee60bacc6b9
UentR <riggiquentin@gmail.com>
2023-11-30 09:50:54 +0100

    Load Tree

--- Guidé/All/loadIA.cpp
+++ Guidé/All/loadIA.cpp
@@ -1,66 +1,45 @@
-#include <iostream>
 #include <fstream>
 #include <string>
-#include <cstring>
 #include <map>
-#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 struct Node
 {
     string Ana;
-    map<string, int *> Children;
+    map<string, Node *> Children;
 };
+
+string Sort(string Mot)
+{
+    string Sorted = Mot;
+    sort(Sorted.begin(), Sorted.end());
+    return Sorted;
+}
 
 int main()
 {
-	
-		Node * Test = new Node;
-		Test->Ana = "Test";
-	int *Temp = new int(10);
-	Test->Children = {{"a", Temp}};
-	cout << Test << endl;
-	cout << Test->Children["a"] << endl;
-
-    /*char *Nom3 = new char[9];
-
-    string Nom2 = "Test";
-
-    for (int i = 0; i < Nom2.length(); i++)
+    ifstream File;
+    File.open("Text/Arbre.txt");
+    Node *T;
+    map<string, Node *> Tree;
+    string letter;
+    int Size;
+    for (int i = 0; i < 124492; i++)
     {
-        Nom[i] = Nom2[i];
+        T = new Node;
+        File >> T->Ana;
+
+        File >> letter;
+        File >> letter;
+
+        while (letter != "]")
+        {
+            T->Children[letter] = Tree[Sort((T->Ana + letter))];
+            File >> letter;
+        }
+
+        Tree[Sort(T->Ana)] = T;
     }
-
-    *Nom3 = *Nom;
-
-    Nom2 = "Test2";
-
-    for (int i = 0; i < Nom2.length(); i++)
-    {
-        Nom[i] = Nom2[i];
-    }
-
-    cout << Nom << endl;
-    cout << Nom3 << endl;
-    */
-    // ifstream File;
-    // File.open("Text/After2.json");
-    // map<string, Node> MAP;
-    // string letter;
-    // vector<int *>
-    // for (int i = 0; i < 124492; i++)
-    // {
-    //     string Nom;
-    //     int Nbr;
-    //     int *Pointer;
-
-    //     Nbr = 0;
-    //     File >> Nom;
-    //     File >> letter;
-    //     Nbr = 0
-    //     while (letter != "]") {
-
-    //     }
-    // }
 }
