9cd8b41b887cf1382b38c94c059fb90821abe35f
UentR <riggiquentin@gmail.com>
2023-12-05 12:45:40 +0100

    Search Tree

--- Guidé/All/controleurChoixAction.cpp
+++ Guidé/All/controleurChoixAction.cpp
@@ -14,11 +14,27 @@ using namespace std;
 
 using BOARD = vector<vector<string>>;
 
+/**
+ * Checks if a letter is a vowel.
+ *
+ * @param Letter The letter to check.
+ * @return True if Letter is 'A', 'E', 'I', 'O', 'U' or 'Y', false otherwise.
+ */
 bool isVowel(string Letter)
 {
     return (Letter == "A" or Letter == "E" or Letter == "I" or Letter == "O" or Letter == "U" or Letter == "Y");
 }
 
+/**
+ * Creates a vector containing all the letters to use for the game,
+ * with letter frequencies based on Scrabble letter distributions.
+ *
+ * Initializes a map with the frequency of each letter.
+ * Populates a vector with the appropriate number of each letter.
+ * Shuffles the vector while ensuring at least one vowel in the first
+ * and second halves.
+ * Returns the shuffled vector of letters.
+ */
 vector<string> createLetters()
 {
     map<string, int> usableLetter = {
@@ -88,6 +104,13 @@ vector<string> createLetters()
 vector<string> LETTERS = createLetters();
 int LEN = LETTERS.size();
 
+/**
+ * Prompts the user to choose a dictionary file to use for the game.
+ *
+ * Prints a message asking the user which dictionary they want to choose.
+ * Reads the user's input into a string variable.
+ * Returns the user's dictionary choice as a string.
+ */
 string choisirDictionnaire()
 {
     string choix;
@@ -96,6 +119,12 @@ string choisirDictionnaire()
     return choix;
 }
 
+/**
+ * Pops the last letter from the global LETTERS vector
+ * containing all letters left in the bag.
+ * Decrements the global LEN counter tracking remaining letters.
+ * Returns the popped letter to the caller.
+ */
 string piocheLettre()
 {
     LEN--;
@@ -103,6 +132,15 @@ string piocheLettre()
     return LETTERS[LEN];
 }
 
+/**
+ * Shuffles the global LETTERS vector containing all letters in the bag.
+ *
+ * Takes the string Rep containing the letters to add back to the bag.
+ * For each letter in Rep:
+ *   - Adds the letter back to the LETTERS vector
+ *   - Increments the global LEN counter tracking number of letters
+ * After adding all letters back, shuffles the LETTERS vector randomly.
+ */
 void shuffleBag(string Rep)
 {
     for (auto i : Rep)
@@ -114,6 +152,16 @@ void shuffleBag(string Rep)
 }
 
 // DOESN'T WORK BUT BORING
+/**
+ * Exchanges 3 letters from the player's letter rack with new letters.
+ *
+ * Checks if the player has at least 3 letters.
+ * Prompts the player to choose 3 letters to exchange.
+ * Validates the 3 letters chosen.
+ * Removes the 3 letters from the player's rack.
+ * Adds 3 new random letters to the player's rack.
+ * Returns the updated board state.
+ */
 BOARD echangeLettre(BOARD Board, int Joueur)
 {
     if (Board[Joueur][0].size() < 3)
@@ -127,7 +175,6 @@ BOARD echangeLettre(BOARD Board, int Joueur)
     {
         cout << "Votre vrac est actuellement : " << Board[Joueur][0] << endl
              << "De quelle lettre voulez-vous vous séparer ?" << endl;
-        string Rep;
         cin >> Rep;
         Rep = purifie(Rep);
         if (Rep.size() != 3)
@@ -153,6 +200,11 @@ BOARD echangeLettre(BOARD Board, int Joueur)
     return Board;
 }
 
+/**
+ * Prints a message to the console showing the player their possible actions during the game.
+ * Actions include abandoning, continuing, exchanging letters, finishing turn,
+ * challenging Jarnac, viewing menu, and viewing board.
+ */
 void ActionPossible()
 {
     cout << "Au cours du jeu il est possible de taper certaines lettres pour piloter la manche." << endl;
@@ -166,6 +218,20 @@ void ActionPossible()
     cout << endl;
 }
 
+/**
+ * Places a word on the board for the given player.
+ *
+ * Updates the player's letter rack, board state, and scores.
+ * Handles normal play or Jarnac (challenging opponent's word).
+ *
+ * @param Board The current board state
+ * @param Joueur The current player index
+ * @param Ligne The board row to place the word on
+ * @param Mot The word to be placed
+ * @param diffLetter The letters used from the player's rack
+ * @param isJarnac True if this is a Jarnac challenge
+ * @return The updated board state
+ */
 BOARD JouerMot(BOARD Board, int Joueur, int Ligne, string Mot, string diffLetter, bool isJarnac)
 {
 
@@ -191,6 +257,12 @@ BOARD JouerMot(BOARD Board, int Joueur, int Ligne, string Mot, string diffLetter
     return Board;
 }
 
+/**
+ * Prompts the user to place a word on the board, handling invalid input.
+ * Allows choosing a line, entering a word, and validating it against the board state and dictionary.
+ * Handles confirmation loops and user cancellation.
+ * Returns the updated board state after successfully placing a word.
+ */
 BOARD PlaceMot(BOARD Board, int Joueur, vector<string> Dico, vector<long> Bornes, int SHIFT, int NBR, bool isJarnac, string Name, vector<string> Names)
 {
     string Say = "Sur quelle ligne souhaitez-vous jouer ?";
@@ -278,6 +350,16 @@ BOARD PlaceMot(BOARD Board, int Joueur, vector<string> Dico, vector<long> Bornes
     return Board;
 }
 
+/**
+ * Prompts the user to choose an action and performs it, alternating turns between players.
+ *
+ * Displays the game board, prompts for an action, and executes it, handling invalid input.
+ * Actions include placing a word (normal or Jarnac mode), exchanging letters,
+ * passing turn, viewing possible actions, and resigning.
+ *
+ * Returns a special string to stop the game loop if the user resigns, otherwise
+ * returns an empty string to continue alternating turns.
+ */
 string choixAction(BOARD Board, int Joueur, vector<string> Dico, vector<long> Bornes, int SHIFT, int NBR, string Name, vector<string> Names)
 {
     string coup;
@@ -326,6 +408,15 @@ string choixAction(BOARD Board, int Joueur, vector<string> Dico, vector<long> Bo
     return "";
 }
 
+/**
+ * Initializes a new game board with the given width and height.
+ *
+ * Creates a BOARD with two empty player boards, initialized with
+ * empty strings. Draws 6 random letters for each player's hand.
+ * Initializes the remaining rows to empty strings.
+ *
+ * Returns the initialized BOARD.
+ */
 BOARD initBoard(int W, int H)
 {
     BOARD Board = {{""}, {""}};
@@ -344,6 +435,20 @@ BOARD initBoard(int W, int H)
     return Board;
 }
 
+/**
+ * lanceLeJeu launches a new game of Jarnac with the given player names.
+ *
+ * It initializes the game board, displays intro screens, and runs
+ * the main game loop, alternating between players until one wins.
+ *
+ * It handles setting up the dictionary, calculating word search bounds,
+ * calling the display and action selection functions for each turn,
+ * and checking for a winning condition.
+ *
+ * joueur0: Name of Player 1
+ * joueur1: Name of Player 2
+ * Name: Name displayed at top of board
+ */
 bool lanceLeJeu(string joueur0, string joueur1, string Name)
 {
     string nomDico = choisirDictionnaire();
@@ -392,6 +497,13 @@ bool lanceLeJeu(string joueur0, string joueur1, string Name)
     return true;
 }
 
+/**
+ * Main entry point for the program.
+ *
+ * Calls lanceLeJeu() to start a new game, passing player names.
+ *
+ * Returns 0 to indicate successful program completion.
+ */
 int main()
 {
     lanceLeJeu("Moi", "Toi", "----");
--- Guidé/All/loadIA.cpp
+++ Guidé/All/loadIA.cpp
@@ -1,14 +1,19 @@
 #include <fstream>
 #include <string>
 #include <map>
+#include <vector>
 #include <algorithm>
+#include <set>
+#include <tuple>
+#include <iostream>
+#include <unistd.h>
 
 using namespace std;
 
 struct Node
 {
     string Ana;
-    map<string, Node *> Children;
+    map<char, Node *> Children;
 };
 
 string Sort(string Mot)
@@ -18,14 +23,92 @@ string Sort(string Mot)
     return Sorted;
 }
 
-int main()
+string fRetire(string Word, char Letter)
+{
+    string newString = "";
+    for (char character : Word)
+    {
+        if (character != Letter)
+        {
+            newString += character;
+        }
+    }
+    return newString;
+}
+
+tuple<Node *, string> Analyze(string Vrac, vector<Node *> PlayerWords)
+{
+    set<tuple<Node *, string, string, Node *>> toAnalyze = {};
+    for (Node *Word : PlayerWords)
+    {
+        toAnalyze.insert(make_tuple(Word, Vrac, "", Word));
+    }
+
+    set<tuple<Node *, string>> End = {};
+
+    Node *Word;
+    string tVrac;
+    string L;
+    bool Child;
+    string Path;
+    Node *Origin;
+    while (!toAnalyze.empty())
+    {
+        tie(Word, tVrac, Path, Origin) = *toAnalyze.begin();
+        toAnalyze.erase(toAnalyze.begin());
+
+        Child = false;
+        // cout << Path.length() << endl;
+        for (char Letter : tVrac)
+        {
+            if (Word->Children.find(Letter) != Word->Children.end())
+            {
+                Child = true;
+                toAnalyze.insert(make_tuple(Word->Children[Letter], fRetire(tVrac, Letter), Path + Letter, Origin));
+            }
+        }
+        if (!Child)
+        {
+            End.insert(make_tuple(Origin, Path));
+        }
+    }
+
+    int Longest = 0;
+    string BestPath;
+    Node *BestStart;
+    for (tuple<Node *, string> EndWord : End)
+    {
+        tie(Word, Path) = EndWord;
+        if (Path.length() > Longest)
+        {
+            Longest = Path.length();
+            BestPath = Path;
+            BestStart = Word;
+        }
+    }
+    return make_tuple(BestStart, BestPath);
+}
+
+/**
+ * Loads a tree data structure from a text file.
+ *
+ * The text file contains words, with child letters separated by spaces,
+ * and different words separated by semicolons.
+ *
+ * This builds up a tree mapping prefixes to child nodes.
+ * It creates Node objects to represent each node in the tree.
+ *
+ * It uses a helper Sort function to sort the letters in each word,
+ * so that anagrams map to the same node.
+ */
+map<string, Node *> LoadTree(string FileName)
 {
     ifstream File;
     Node *T;
     map<string, Node *> Tree;
     string letter;
 
-    File.open("Text/Arbre.txt");
+    File.open(FileName);
     T = new Node;
 
     while (File >> T->Ana)
@@ -35,7 +118,7 @@ int main()
 
         while (letter != ";")
         {
-            T->Children[letter] = Tree[Sort((T->Ana + letter))];
+            T->Children[letter[0]] = Tree[Sort((T->Ana + letter))];
             File >> letter;
         }
 
@@ -43,4 +126,51 @@ int main()
         T = new Node;
     }
     File.close();
+    return Tree;
+}
+
+int main()
+{
+    map<string, Node *> Tree = LoadTree("Text/Arbre.txt");
+    cout << "Tree loaded" << endl;
+    vector<string> WORDS = {
+        "OUT",
+        "OSA",
+        "TON",
+        "ZEN",
+        "NON",
+        "MEO",
+        "SKI",
+        "RIE"};
+    vector<Node *> PlayerWords = {};
+    for (string Word : WORDS)
+    {
+        PlayerWords.push_back(Tree[Sort(Word)]);
+    }
+
+    cout << "Player words loaded" << endl;
+
+    // 5s pause
+    for (int i = 0; i < 5; i++)
+    {
+        cout << i << endl;
+        sleep(1);
+    }
+
+    string Vrac = "UGYFT";
+    Node *Origin;
+    string Path;
+    tie(Origin, Path) = Analyze(Vrac, PlayerWords);
+    cout << "Longest path: " << Path << endl;
+    cout << "Longest path length: " << Path.length() << endl;
+    cout << "Origin: " << Origin->Ana << endl;
+
+    cout << "To place:" << endl;
+    Node *Temp = Origin;
+    cout << Temp->Ana << endl;
+    for (char Letter : Path)
+    {
+        Temp = Temp->Children[Letter];
+        cout << Temp->Ana << endl;
+    }
 }
